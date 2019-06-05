69b7a4cf3311ffa388b766a7e58442db79ee7fec
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 20:43:39 -0600

    Add files via upload

---
+++ Ejercicio11.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Imprimir, contar y sumar los multiplos de 2 hasta un número introducido por teclado
+            int suma=0, cnt = 0, valor;
+
+
+            Console.Write("Ingrese número: ");
+            valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i++)
+            {
+                if (i%2==0)
+                {
+                    Console.Write(i + " ");
+                    cnt++;
+                    suma = suma + i;
+                }
+            }
+            Console.Write("\n ");
+            Console.Write("El total de la suma es: "+ suma+ " \n");
+            Console.Write("Hay " + cnt + " multiplos entre el 1 al "+ valor);
+            Console.ReadLine();
+        }
+    }
+}
