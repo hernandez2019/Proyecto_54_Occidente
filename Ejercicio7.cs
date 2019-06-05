4c407eca1aae43f272bdb1c461e6df07323c8d7d
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 20:19:25 -0600

    Add files via upload

---
+++ Ejercicio7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un algoritmo que imprima todos los numeros naturales que hay desde la unidad
+            //hasta un número introducido por teclado.
+
+            int valor;
+            Console.Write("Ingrese un número: ");
+            valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
