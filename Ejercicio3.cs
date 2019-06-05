d0a98028eba5e1d1acd0370f1688a81c8c09d5af
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 19:02:39 -0600

    Add files via upload

---
+++ Ejercicio3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un algoritmo que imprima las sumas de los primeros 100 números 
+            int suma = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                suma = suma + i;
+            }
+            Console.Write("El resultado de la suma de los primeros 100 números es: " + suma);
+            Console.ReadLine();
+        }
+    }
+}
