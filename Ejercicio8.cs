625c98cd6fe12c4c55bf40f83f1dc49a601cdd60
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 20:22:56 -0600

    Add files via upload

---
+++ Ejercicio8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un algoritmo que imprima y cuente los multiplos de 3 que hay entre 1 y 100
+            for (int i = 1; i <=100 ; i++)
+            {
+                if (i%3==0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
