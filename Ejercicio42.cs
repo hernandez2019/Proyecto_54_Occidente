1ad5e6948c7f2e0521c7be6f466763b92750e97a
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 23:21:19 -0600

    Add files via upload

---
+++ Ejercicio42.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implementar un método en java que reciba como parámetro un vector de 20 elementos
+            int[] Vector1 = new int[19];
+            int conteo = 0;
+
+            for (int i = 0; i < Vector1.Length; i++)
+            {
+                conteo = i;                
+            }
+            if (conteo==20)
+            {
+                Vector(Vector1);
+            }
+
+        }
+        public static void Vector( int[] intArray)
+        {
+            
+
+        }
+    }
+}
