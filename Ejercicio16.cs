74aa59e95c52eb381c3ae0bcbef97ad7e92ad28d
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 21:52:54 -0600

    Add files via upload

---
+++ Ejercicio16.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introducir un número por teclado que nos diga si es positivo o negativo
+            int numero;
+            Console.Write("Ingrese número: ");
+            numero = int.Parse(Console.ReadLine());
+            if (numero >=0)
+            {
+                Console.Write("El número " + numero + " es positivo");
+            }
+            else
+            {
+                Console.Write("El número " + numero + " es negativo");
+            }
+            Console.ReadLine();
+        }
+    }
+}
