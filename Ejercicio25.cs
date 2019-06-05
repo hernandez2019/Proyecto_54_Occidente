c232648226a7a9c834f68553d45f988e1ac83e73
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 22:32:59 -0600

    Add files via upload

---
+++ Ejercicio25.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introducir tantos números que hay entre el 1 al 100.excepto los múltiplos de 2.
+            int numero = 0;
+            for (int i = 1; i < 15; i++)
+            {
+                Console.Write("Ingrese un numero entre el 1 al 100, excepto los Multiplos de 2:");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (numero<0 || numero >100)
+                {
+                    Console.WriteLine("El numero ingresado no esta entre el rango de 1 - 100");
+                    Console.ReadKey();
+                    return;
+                }
+                if (numero %2==0)
+                {
+                    Console.WriteLine("El numero ingresado es multiplo de dos");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+        }
+    }
+}
