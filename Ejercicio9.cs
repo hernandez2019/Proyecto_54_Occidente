fed145f9d6e04bda095772a4facdacf8abdb89e5
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 20:34:15 -0600

    Add files via upload

---
+++ Ejercicio9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un algoritmo que calcule la suma de los números impares conprendidos entre el 0 y el 100
+            int suma_impar = 0;
+            for (int i = 0; i <=100 ; i++)
+            {
+                if (i%3==0)
+                {
+                    suma_impar = suma_impar + i;
+                }
+            }
+            Console.Write("La suma de los impares es: " + suma_impar);
+            Console.ReadLine();
+        }
+    }
+}
