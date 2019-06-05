c2d8310707e4070dc796bde3ee6cbc24e79fadde
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 19:07:38 -0600

    Add files via upload

---
+++ Ejercicio4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un algoritmo que imprima los números pares entrel el 0 al 100
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i%2==0){
+                    Console.Write(i +" ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
