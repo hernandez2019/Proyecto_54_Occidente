2bc0c6d299e1539e403274749e621cdd74dcd201
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 22:28:11 -0600

    Add files via upload

---
+++ Ejercicio21.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Imprimir y contar los números que son múltiplos de 2 y 3 que hay del 1 al 100.
+            int cant = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                    cant = cant + 1;
+                }
+            }            Console.WriteLine("Multiplos de 2 y 3 encontrados: {0}", cant);            Console.ReadKey();
+        }
+    }
+}
