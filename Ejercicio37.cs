78a658c57a92afb584e5619ec5583f76ae9b571b
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 23:14:47 -0600

    Add files via upload

---
+++ Ejercicio37.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer un programa que determine cuantas cifras posee un número
+            //entero positivo introducido por teclado.repetir el proceso
+            int numero = 0, cant = 0;
+            Console .Write ("Ingrese un numero entero positivo: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (numero >= 10)
+            {
+                cant++;
+                numero = numero / 10;
+            }
+            Console .WriteLine ("El número tiene: " +(cant + 1) + " dígitos");
+            Console.ReadKey();
+
+        }
+    }
+}
