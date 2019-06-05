cc51a10bc0f80a2af785bc06bb8498df6ca47633
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 23:17:30 -0600

    Add files via upload

---
+++ Ejercicio38.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular la suma de los n primeros términos de una progresión aritmética,
+            //dada por el valor de su primer termino y la diferencia entre dos términos consecutivos cualesquiera
+
+
+        }
+    }
+}
