e3e6c16157ce32f565157c53c3e964f7fe675673
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 21:40:25 -0600

    Add files via upload

---
+++ Ejercicio14.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular la suma de los cuadrados de los números pares entre el 0 y el 100
+            double suma,total=0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i%2==0)
+                {
+                  suma = Math.Pow(i, 2);
+                  total = total + suma;
+                    Console.Write(i+" ");
+                }
+            }
+            Console.Write("\nEl total de la suma en potencias es: " + total);
+            Console.ReadLine();
+        }
+    }
+}
