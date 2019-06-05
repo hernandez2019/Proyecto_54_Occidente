487e8e99c39513baaf3bd57695bd8c83c3672a1b
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 22:48:43 -0600

    Add files via upload

---
+++ Ejercicio35.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Obtener el cociente y el resto de dos números enteros positivos mediante restas sucesivas
+            int n1 = 0, n2 = 1, c = 0, i = 0;
+            while (n2 > n1)
+            {
+                Console .Write("Ingrese un valor entero positivo: ");
+                n1 =Convert .ToInt32 (Console .ReadLine ());
+                Console .Write("Ingrese el otro valor menor al anterior: ");
+                n2 = Convert.ToInt32(Console.ReadLine());
+            }
+            c = n1;
+            while (c >= n2)
+            {
+                c = c - n2;
+                i++;
+            }
+            Console .WriteLine("El cociente por restas sucesivas es: " +i);
+            Console .WriteLine("El resto de la divisón por restas sucesivas es: " +c);
+            Console.ReadKey();
+
+        }
+    }
+}
