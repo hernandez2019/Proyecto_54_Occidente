65ffd961d4c5906818be31ddacf5fcb6d50e6550
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 23:19:24 -0600

    Add files via upload

---
+++ Ejercicio40.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular el menor numero primo de un numero que introducimos vía teclado.
+            int c = 0, aux = 0, cp = 0, numero = 0;
+            Console .Write("Ingrese un valor: ");
+            numero = Convert .ToInt32 (Console .ReadLine ());
+            while (c < numero)
+            {
+                c++;
+                while (aux < c)
+                {
+                    aux++;
+                    if (c % aux == 0)
+                    {
+                        cp++;
+                    }
+                }
+                if (cp == 2)
+                {
+                    Console .WriteLine(" "+c);
+
+                }
+                cp = 0;
+                aux = 0;
+            }
+            Console .WriteLine("El menor numero primos es: 2");
+            Console.ReadKey();
+
+        }
+    }
+}
