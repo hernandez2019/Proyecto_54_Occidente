9cce4710eed70376568f4a7c7d018bb04cd90d1b
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 20:18:42 -0600

    Add files via upload

---
+++ Ejercicio6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma_impar = 0;
+            for (int i = 100; i >=1; i--)
+            {
+
+                if (i %2!=0)
+                {
+                    // Hacer un algoritmo que imprima los números impares desde el 100 hasta la unidad y calcule su suma
+                    Console.Write(i + " ");
+                    suma_impar = suma_impar + i;
+                }
+
+            }
+            Console.Write("\nLa suma de los impares es: " + suma_impar);
+            Console.ReadLine();
+        }
+    }
+}
