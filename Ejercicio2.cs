46838746f3c810cad32d3b46f35ba43167cd9759
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 18:58:34 -0600

    Add files via upload

---
+++ Ejercicio2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace del_100_al_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer un algoritmo que imprima los número del 100 al 0;
+            for (int i = 100; i >= 0; i--)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
