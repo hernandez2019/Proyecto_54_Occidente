4a2bdddea11182fb35f04a5f462ee92903e1bd02
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 21:56:38 -0600

    Add files via upload

---
+++ Ejercicio17.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introducir un número por teclado. que nos diga si es par o impar
+            int numero;
+            Console.Write("Ingrese número: ");
+            numero = int.Parse(Console.ReadLine());
+            if (numero%2== 0)
+            {
+                Console.Write("El número " + numero + " es par");
+            }
+            else
+            {
+                Console.Write("El número " + numero + " es impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
