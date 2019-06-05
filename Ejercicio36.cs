b27953ae262ee7ca01822d3451e54fb21c92fe7b
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 22:49:38 -0600

    Add files via upload

---
+++ Ejercicio36.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer un algoritmo que calcule las raíces de una ecuación de segundo grado
+            //Repetir el proceso tantas veces como se desee.
+
+        }
+    }
+}
