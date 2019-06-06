9c9b432a0be251dad6a23e18744c61237d9b354a
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-05 23:44:03 -0600

    Add files via upload

---
+++ Ejercicio47.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un algoritmo que calcule independientemente las sumas de impares de los números entre el 1 y el 1000.
+            int sumaimp = 0;
+            for (int i = 1; i <=1000; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sumaimp = sumaimp + i;                   
+                }
+               
+            }
+            Console .WriteLine ("La suma de los impares es: {0}",sumaimp);
+            Console.ReadKey();
+
+        }
+    }
+}
